443e602da38cfa258f57a8ca6a9f35eed5196b0f
deviator <code.viator@gmail.com>
2015-02-05 01:02:25 +0300

    opop

--- src/draw.d
+++ src/draw.d
@@ -1,10 +1,13 @@
 module draw;
 
+import std.conv : to;
+
 import des.gl;
 import des.space;
 import des.math.linear;
 import des.util.logsys;
 import des.util.helpers;
+import des.util.stdext.algorithm;
 
 import loader;
 
@@ -20,30 +23,71 @@ out vec4 ocolor;
 void main(void) { ocolor = vec4(1,0,0,1); }
 `;
 
-class TestDraw : GLSimpleObject, SpaceNode
+class DrawMesh : GLSimpleObject, SpaceNode
 {
     mixin SpaceNodeHelper;
 
 protected:
 
-    GLBuffer pos;
-    GLBuffer ind;
+    GLBuffer vertices;
+    GLBuffer normals;
+    GLBuffer[] texcrds;
+    GLBuffer indices;
+
+    abstract void prepareAttribPointers();
 
 public:
 
+    this( string shader_src, MeshData md )
+    {
+        super( shader_src );
+
+        vertices = createArrayBuffer();
+        vertices.setData( md.vertices );
+
+        if( md.normals.length )
+        {
+            normals = newEMM!GLBuffer();
+            normals.setData( md.normals );
+        }
+
+        foreach( i; 0 .. md.texcrds.length )
+        {
+            auto tbuf = newEMM!GLBuffer();
+            switch( md.texcrdsdims[i] )
+            {
+                case 1:
+                    tbuf.setData( amap!(a=>a.x)( md.texcrds[i] ) );
+                    break;
+                case 2:
+                    tbuf.setData( amap!(a=>a.xy)( md.texcrds[i] ) );
+                    break;
+                case 3:
+                    tbuf.setData( md.texcrds[i] );
+                    break;
+                default:
+                    throw new Exception( "WTF?: texture coordinate dims == " ~ 
+                            to!string( md.texcrdsdims[i] ) );
+            }
+            texcrds ~= tbuf;
+        }
+
+        if( md.indices.length )
+        {
+            indices = createIndexBuffer();
+            indices.setData( md.indices );
+        }
+
+        prepareAttribPointers();
+    }
+}
+
+class TestDraw : DrawMesh
+{
     this()
     {
-        super( SS_TEST );
-
         auto ll = new Loader( appPath( "..", "data", "abstract_model.dae" ) );
-
-        pos = createArrayBuffer();
-        auto loc = shader.getAttribLocation( "pos" );
-        setAttribPointer( pos, loc, 3, GLType.FLOAT );
-        pos.setData( ll.vertices );
-
-        ind = createIndexBuffer();
-        ind.setData( ll.indices );
+        super( SS_TEST, ll.meshes[0] );
     }
 
     void draw( Camera cam )
@@ -52,5 +96,13 @@ public:
 
         glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
         drawArrays( DrawMode.TRIANGLES );
+    }
+
+protected:
+
+    override void prepareAttribPointers()
+    {
+        auto loc = shader.getAttribLocation( "pos" );
+        setAttribPointer( vertices, loc, 3, GLType.FLOAT );
     }
 }
